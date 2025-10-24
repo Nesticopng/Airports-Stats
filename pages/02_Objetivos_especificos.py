502f12e7c4094029f5dca24cbc6aea9e192f44cc
Angel Velasquez <ADVP1036@HOTMAIL.COM>
2025-10-23 23:46:39 -0400

    Update 02_Objetivos_especificos.py

--- pages/02_Objetivos_especificos.py
+++ pages/02_Objetivos_especificos.py
@@ -15,6 +15,13 @@ st.markdown("---")
 
 # Tab 1: Datos Completos
 st.subheader("Objetivos específicos")
-st.markdown("---")
-st.write(""".
-""")
+objetivos = """
+- Estudiar el comportamiento de la cantidad de pasajeros nacionales e internacionales en los aeropuertos de Estados Unidos en los años 2022-2023
+- Calcular las estadísticas descriptivas del número de pasajeros por aeropuerto en los años 2022-2023
+- Analizar la proporción de vuelos nacionales vs internacionales por aeropuerto
+- Identificar aeropuertos con mayor y menor cantidad de pasajeros en cada categoría
+- Determinar los 10 aeropuertos que tuvieron el mayor porcentaje de crecimiento en los años 2022-2023
+- Comparar el ranking de aeropuertos con mayor cantidad de pasajeros entre 2022 y 2023
+"""
+
+st.markdown(objetivos)
